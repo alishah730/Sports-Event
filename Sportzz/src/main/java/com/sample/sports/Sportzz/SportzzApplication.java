92c7904b5fd512d3e39605858be321fc96fe15bc
Ali <habibul.shah@ge.com>
2017-07-11 11:38:35 +0530

    Initital Code base
    Creating code base for Sports web applications

---
+++ Sportzz/src/main/java/com/sample/sports/Sportzz/SportzzApplication.java
@@ -0,0 +1,12 @@
+package com.sample.sports.Sportzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportzzApplication.class, args);
+	}
+}
---
+++ Sportzz/src/test/java/com/sample/sports/Sportzz/SportzzApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sample.sports.Sportzz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportzzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
